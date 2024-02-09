7060ac781515e878bd6eaa89be8d43a850d5de37
ScarletStranger <maksimledenev6@gmail.com>
2024-02-09 19:49:19 +0300

    optimized code

--- LetterCount/main.go
+++ LetterCount/main.go
@@ -15,34 +15,7 @@ func main() {
 
 	text := string(file)
 	letterCount := 0
-	letters := map[rune]int{
-		'a': 0,
-		'b': 0,
-		'c': 0,
-		'd': 0,
-		'e': 0,
-		'f': 0,
-		'g': 0,
-		'h': 0,
-		'i': 0,
-		'j': 0,
-		'k': 0,
-		'l': 0,
-		'm': 0,
-		'n': 0,
-		'o': 0,
-		'p': 0,
-		'q': 0,
-		'r': 0,
-		's': 0,
-		't': 0,
-		'u': 0,
-		'v': 0,
-		'w': 0,
-		'x': 0,
-		'y': 0,
-		'z': 0,
-	}
+	letters := map[rune]int{}
 
 	for _, v := range strings.ToLower(strings.ReplaceAll(text, " ", "")) {
 		if unicode.IsLetter(v) {
@@ -51,9 +24,7 @@ func main() {
 		}
 	}
 
-	for a, b := range letters {
-		if b > 0 {
-			fmt.Printf("%c: %d %f\n", a, b, float32(letterCount/letters[b]))
-		}
+	for key, value := range letters {
+		fmt.Printf("%c - %d %.2f\n", key, value, float32(value/letterCount))
 	}
 }
